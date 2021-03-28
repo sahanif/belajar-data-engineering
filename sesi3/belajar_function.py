b9e46701b9fb028f73011bd2997c1999ac1cf306
sahanif <45937359+sahanif@users.noreply.github.com>
2021-03-28 13:51:39 +0700

    Delete sesi3 directory

--- sesi3/belajar_function.py
+++
@@ -1,6 +0,0 @@
-def movie_stats (judul,jumlah, harga_tiket,*kota):
-    pendapatan = jumlah*harga_tiket
-    print ("Tiket film " , judul," terjual sebanyak", jumlah," dengan total pendapatan Rp.",pendapatan," di kota ",kota)
-
-movie_stats ("Spiderman", 100, 50000, "Jakarta, Surabaya" )
-movie_stats ("Ironman",300,10000,"Jakarta, Surabaya, Malang")
